041eec96a3eb5bdb723a59a3f763759269e94816
Filippos Christianos <fchristianos@ed.ac.uk>
2020-02-15 12:01:21 +0000

    Simplifies pre-registered environments

--- robotic_warehouse/__init__.py
+++ robotic_warehouse/__init__.py
@@ -2,29 +2,61 @@ import gym
 from .warehouse import Warehouse, RewardType, Action
 import itertools
 
-_perms = itertools.product(
-    range(1, 5), range(3, 10, 2), range(1, 20), range(1, 20), ["indiv", "global", "twostage"]
-)
-_rewards = {
-    "indiv": RewardType.INDIVIDUAL,
-    "global": RewardType.GLOBAL,
-    "twostage": RewardType.TWO_STAGE,
+_sizes = {
+    "tiny": (1, 3),
+    "small": (2, 3),
+    "medium": (2, 5),
+    "large": (3, 5),
 }
 
-for rows, cols, agents, req, rew in _perms:
+_perms = itertools.product(
+    _sizes.keys(),
+    range(1, 20),
+)
+
+for size, agents in _perms:
     gym.register(
-        id=f"rware-{rows}x{cols}-{agents}ag-{req}req-{rew}-v0",
+        id=f"rware-{size}-{agents}ag-v0",
         entry_point="robotic_warehouse.warehouse:Warehouse",
         kwargs={
             "column_height": 8,
-            "shelf_rows": rows,
-            "shelf_columns": cols,
+            "shelf_rows": _sizes[size][0],
+            "shelf_columns": _sizes[size][1],
             "n_agents": agents,
             "msg_bits": 0,
             "sensor_range": 1,
-            "request_queue_size": req,
+            "request_queue_size": agents,
             "max_inactivity_steps": None,
             "max_steps": 500,
-            "reward_type": _rewards[rew],
+            "reward_type": RewardType.INDIVIDUAL,
         },
     )
+
+
+def full_registration():
+    _perms = itertools.product(
+        range(1, 5), range(3, 10, 2), range(1, 20), range(1, 20), ["indiv", "global", "twostage"]
+    )
+    _rewards = {
+        "indiv": RewardType.INDIVIDUAL,
+        "global": RewardType.GLOBAL,
+        "twostage": RewardType.TWO_STAGE,
+    }
+
+    for rows, cols, agents, req, rew in _perms:
+        gym.register(
+            id=f"rware-{rows}x{cols}-{agents}ag-{req}req-{rew}-v0",
+            entry_point="robotic_warehouse.warehouse:Warehouse",
+            kwargs={
+                "column_height": 8,
+                "shelf_rows": rows,
+                "shelf_columns": cols,
+                "n_agents": agents,
+                "msg_bits": 0,
+                "sensor_range": 1,
+                "request_queue_size": req,
+                "max_inactivity_steps": None,
+                "max_steps": 500,
+                "reward_type": _rewards[rew],
+            },
+        )
