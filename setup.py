fb2c706a433cd1dab6ac67a573d9e8dc565630bf
Lukas Schaefer <l.schaefer@ed.ac.uk>
2020-02-18 18:18:47 +0000

    update gym version to at least 0.15 (need updated Tuples with index access defined)

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
     ],
-    install_requires=["numpy", "gym>=0.12", "pyglet", "networkx"],
+    install_requires=["numpy", "gym>=0.15", "pyglet", "networkx"],
     extras_require={"test": ["pytest"]},
     include_package_data=True,
 )
