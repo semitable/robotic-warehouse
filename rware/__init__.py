cebb3cb0c4cb18c4af7918812838700d18dd065c
Lukas Schaefer <luki.schaefer96@gmail.com>
2021-10-24 21:26:59 +0100

    Adding registration of environments with varying column heights (#3)
    * add registration of rware tasks with variable column height
    
    * move registration of alternative column height all to full registration

--- rware/__init__.py
+++ rware/__init__.py
@@ -32,11 +32,31 @@ for size, diff, agents in _perms:
         },
     )
 
-
 def full_registration():
+    _perms = itertools.product(_sizes.keys(), _difficulty, range(1, 20), range(2, 11),)
+    for size, diff, agents, column_height in _perms:
+        # normal tasks with modified column height
+        gym.register(
+            id=f"rware-{size}-{column_height}h-{agents}ag{diff}-v1",
+            entry_point="rware.warehouse:Warehouse",
+            kwargs={
+                "column_height": column_height,
+                "shelf_rows": _sizes[size][0],
+                "shelf_columns": _sizes[size][1],
+                "n_agents": agents,
+                "msg_bits": 0,
+                "sensor_range": 1,
+                "request_queue_size": int(agents * _difficulty[diff]),
+                "max_inactivity_steps": None,
+                "max_steps": 500,
+                "reward_type": RewardType.INDIVIDUAL,
+            },
+        )
+
     _perms = itertools.product(
         range(1, 5),
         range(3, 10, 2),
+        range(2, 11),
         range(1, 20),
         range(1, 20),
         ["indiv", "global", "twostage"],
@@ -47,12 +67,12 @@ def full_registration():
         "twostage": RewardType.TWO_STAGE,
     }
 
-    for rows, cols, agents, req, rew in _perms:
+    for rows, cols, column_height, agents, req, rew in _perms:
         gym.register(
-            id=f"rware-{rows}x{cols}-{agents}ag-{req}req-{rew}-v0",
+            id=f"rware-{rows}x{cols}-{column_height}h-{agents}ag-{req}req-{rew}-v1",
             entry_point="rware.warehouse:Warehouse",
             kwargs={
-                "column_height": 8,
+                "column_height": column_height,
                 "shelf_rows": rows,
                 "shelf_columns": cols,
                 "n_agents": agents,
