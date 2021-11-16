c66bb31a6e3c71c8e56f19804e7090df9bcb0178
Lukas Schaefer <luki.schaefer96@gmail.com>
2021-11-16 13:23:39 +0000

    add new registration of basic image observation rware envs

--- rware/__init__.py
+++ rware/__init__.py
@@ -32,6 +32,38 @@ for size, diff, agents in _perms:
         },
     )
 
+
+def image_registration():
+    _observation_type = {"": ObserationType.FLATTENED, "-img": ObserationType.IMAGE}
+    _image_directional = {"": True, "-Nd": False}
+    _perms = itertools.product(_sizes.keys(), _difficulty, _observation_type, _image_directional, range(1, 20),)
+    for size, diff, obs_type, directional, agents in _perms:
+        if obs_type == "" and directional == "":
+            # already registered before
+            continue
+        if directional != "" and obs_type == "":
+            # directional values should only be used with image observations 
+            continue
+        gym.register(
+            id=f"rware{obs_type}{directional}-{size}-{agents}ag{diff}-v1",
+            entry_point="rware.warehouse:Warehouse",
+            kwargs={
+                "column_height": 8,
+                "shelf_rows": _sizes[size][0],
+                "shelf_columns": _sizes[size][1],
+                "n_agents": agents,
+                "msg_bits": 0,
+                "sensor_range": 1,
+                "request_queue_size": int(agents * _difficulty[diff]),
+                "max_inactivity_steps": None,
+                "max_steps": 500,
+                "reward_type": RewardType.INDIVIDUAL,
+                "observation_type": _observation_type[obs_type],
+                "image_observation_directional": _image_directional[directional],
+            },
+        )
+
+
 def full_registration():
     _observation_type = {"": ObserationType.FLATTENED, "-img": ObserationType.IMAGE}
     _sensor_ranges = {f"-{sight}s": sight for sight in range(2, 6)}
@@ -40,7 +72,7 @@ def full_registration():
     _perms = itertools.product(_sizes.keys(), _difficulty, _observation_type, _sensor_ranges, _image_directional, range(1, 20), range(2, 16),)
     for size, diff, obs_type, sensor_range, directional, agents, column_height in _perms:
         # normal tasks with modified column height
-        if directional != "" and _observation_type == "":
+        if directional != "" and obs_type == "":
             # directional should only be used with image observations 
             continue
         gym.register(
